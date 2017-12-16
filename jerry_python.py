8c350da16be2312b0e19ca0bae4b667891f40671
Yan lu <yan@wifihost-103-60.wireless.utsc.utoronto.ca>
2017-12-16 12:42:05 -0500

    LAST one

--- jerry_python.py
+++ jerry_python.py
@@ -1,10 +1,9 @@
 
 print("ABCDEFGasfsafac9u5234723u48y32192u32HIGKLMN")
 print("ABCDEFGH
-print("sadasf")
+pri!!!!!!!!!!!!!!!!!!!!!!!!!!
 =print("dsa111")fsaf
 after the change
 print(i+1)
 print("qq111111111111")
 print("FRI\FRIFR")print ;;;;
-dasldsadasdasdsa

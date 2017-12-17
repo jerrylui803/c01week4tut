040758beae3f50eebd06d8cc214b448b9a6bad8c
Qi Yan Lu <luqi2@IITS-B473-31.utsc-labs.utoronto.ca>
2017-12-17 13:29:20 -0500

    some change

--- jerry_python.py
+++ jerry_python.py
@@ -4,10 +4,7 @@ print("ABCDEFGHaaaaaaaaaaaaa111")fsaf
 after the change
 print(i+1)
 i = i + 2
-print(i)
-print(i+1)
+print(i)sdadddd
+print(i+1)dsadsad
 print(i+2)
 print("qq111111111111")
-print("FRI\FRIFR")print ;;;;
-dasldsadasdasdsa
-你好陆琪延v好陆琪延好陆琪延好陆琪延好陆琪延好陆琪延好陆琪延
